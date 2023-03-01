ee593107b8fbbc7e0d804b54d0291a16afeae5ad
MFJess <jessica2001mf@gmail.com>
2023-03-01 11:40:13 +0000

    Fix errors. Working now.

--- tpc2/somas.py
+++ tpc2/somas.py
@@ -1,49 +1,30 @@
 sum = 0
-on = True
+on = False
 digits = ['0','1','2','3','4','5','6','7','8','9']
 controller = ""
-
-def test_off():
-    if ("off" in controller.lower()):
-        on = False
-
-def test_on():
-    if("on" in controller.lower()):
-        on = True
-
-def turn_off():
-    on = False
-    controller = ""
-    print('sum is now off')
-
-def turn_on():
-    on = True
-    controller = ""
-    print('sum is now on')
-
-def switch():
-    if(on and test_off):
-        turn_off()
-    elif(not on and test_on):
-        turn_on()
-
     
 
 string = input('type something\n')
 for char in string:
     if (char == '='):
-            print('sum equals ' + str(sum))
+        print('sum equals ' + str(sum))
 
     elif (char.lower() in "ofn"):
-            controller + char
-            print("antes" + controller)
-            switch()     
-            print("depois" + controller)
+        controller = controller + str(char.lower())
+        if("on" in controller):
+            on = True
+            print("now on")
+            controller = ""
+        if("off" in controller):
+            on = False
+            print("now off")
+            controller = ""
 
     elif (on and char in digits):
         sum += int(char)
+        print("somei " + char)
         
             
         
 
-# test = 182on5=0off1701=2On103
+# test = 182on5=0off1701=2on103=
